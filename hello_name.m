dc98c2afb5812d81ef70fa827dc431360d26bb14
AliZainoul <Ali.Zainoul.az@gmail.com>
2022-09-26 17:29:55 +0200

    Modify hello_name.m

--- hello_name.m
+++ hello_name.m
@@ -1,5 +1,10 @@
 function [greetings] = hello_name ( name )
-if ~isempty(name)
-    myHello = "Hello ";
-    greetings = strcat(myHello, name, "!");
+    if ~isempty(name)
+        myHello = "Hello ";
+        % strcat
+        % input: s_1, ..., s_n of type: char | cell | string
+        % output: outputString of type: string
+        % See: https://fr.mathworks.com/help/matlab/ref/strcat.html
+        greetings = strcat(myHello, name, "!");
+    end
 end
