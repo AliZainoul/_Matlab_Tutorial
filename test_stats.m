62dd62ee2be728bb20464206b81992bc451dd1c7
AliZainoul <Ali.Zainoul.az@gmail.com>
2022-09-27 17:26:40 +0200

    Modifying test_stats and adding stats2.m

--- test_stats.m
+++ test_stats.m
@@ -1,6 +1,6 @@
 % Writing External functions, they have to be in the same directory
 % e.g /Users/home/Documents/MATLAB/Examples/matlab/_matlab_etc_ac
-%     calculateAverage.m | my_mean.m |  test_mean.m
+%     stats.m | test_stats.m
 
 myVector1 = [1 2 3 4]               % average = 10 / 4 = 2.5
 myVector2 = [2.0 5.2 4.8]           % average = 12.0 / 3 = 4.0
