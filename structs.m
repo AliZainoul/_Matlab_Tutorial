82347fe5a3b4a83360cb1b2ce618ee9a56d8300f
AliZainoul <Ali.zainoul.az@gmail.com>
2024-04-11 13:46:09 +0200

    Update structs.m

--- structs.m
+++ structs.m
@@ -4,9 +4,9 @@ person1.age = 30;
 person1.gender = 'Male';
 
 % Accessing struct fields
-fprintf('Name: %s\n', person1.name);
-fprintf('Age: %d\n', person1.age);
-fprintf('Gender: %s\n', person1.gender);
+%fprintf('Name: %s\n', person1.name);
+%fprintf('Age: %d\n', person1.age);
+%fprintf('Gender: %s\n', person1.gender);
 
 % Display the struct
 % disp(person1);
@@ -16,7 +16,7 @@ person1.age = 35;
 person1.gender = 'Female';
 
 % Display the struct
-disp(person1);
+% disp(person1);
 
 % Define a nested struct
 address1.street = '123 Main St';
@@ -26,10 +26,10 @@ address1.zipcode = '12345';
 person1.address = address1;
 
 % Display the nested struct
-disp(person1);
+%disp(person1);
 
-person2 = struct("name",'Ali ZAINOUL', "age", 30, "gender", 'Male')
-disp(person2);
+person2 = struct("name",'Ali ZAINOUL', "age", 30, "gender", 'Male');
+%disp(person2);
 % Define a nested struct
 address2.street = 't';
 address2.city = 'Casablanca';
@@ -39,9 +39,39 @@ person2.address = address2;
 
 arrayTable = table(person1, person2);
 
+
+%{
 for i = 1 : width(arrayTable)
     disp(arrayTable.("person"+num2str(i)).name)
     disp(arrayTable.("person"+num2str(i)).age)
     disp(arrayTable.("person"+num2str(i)).gender)
     disp(arrayTable.("person"+num2str(i)).address)
 end
+%}
+
+
+%{
+for i = 1 : width(arrayTable)
+    currentPerson = arrayTable(:,i).Variables;
+    displayPersonInfo(currentPerson)
+end
+
+function displayPersonInfo(currentPerson)
+    disp(currentPerson.name)
+    disp(currentPerson.age) 
+    disp(currentPerson.gender) 
+    disp(currentPerson.address)
+end
+%}
+
+
+displayField = @(x) disp(x);
+for i = 1 : width(arrayTable)
+    disp(class(arrayTable(:,i)))
+
+    currentPerson = arrayTable(:,i).Variables;
+    structfun(displayField, currentPerson)
+end
+
+
+
