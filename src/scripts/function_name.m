03d3282b8a6e6537dd1b7f87a3c695918f93caa3
AliZainoul <ali.zainoul.az@gmail.com>
2025-11-11 15:38:19 +0100

    UPD fun

---
+++ src/scripts/function_name.m
@@ -0,0 +1,3 @@
+% function [output_1, ..., output_n] = function_name(input_1, ..., input_n)
+%     Instructions
+% end
--- src/scripts/function_signature.m
+++
@@ -1,3 +0,0 @@
-% function [output_1, ..., output_n] = function_signature(input_1, ..., input_n)
-%     Instructions
-% end
